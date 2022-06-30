6b65e77301b2fd3313bef7be8a66596f43b7e213
Ana Kely Lopes <kely@ufc.com.br>
2022-06-30 15:31:17 -0300

    Add Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,30 @@
+import styles from './Post.module.css'
+
 export function Post(){
   return(
-    <div>
-      <h1>Post</h1>
-    </div>
+    <article className={styles.post}>
+      <header>
+       <div className={styles.author}>
+         <img className={styles.avatar} src="https://github.com/kelyAna.png"/>
+         <div className={styles.authorInfo}>
+          <strong>Ana Kely</strong>
+          <span>Software Engineer</span>
+         </div>
+       </div>
+       
+       <time title="29 de junho, às 11:30" dateTime="2022-06-29 11:30:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="#">jane.design/doctorcare</a></p>
+        <p>
+          <a href="#">#novoprojeto</a>{' '}
+          <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>{' '}
+        </p>
+      </div>
+    </article>
   )
 }
