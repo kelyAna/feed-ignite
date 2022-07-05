b04ab05a8f0af9fa84fd7ceee86d2ed3fd26e9d5
Ana Kely Lopes <kely@ufc.com.br>
2022-07-04 23:10:26 -0300

    Add avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -12,7 +13,7 @@ export function Sidebar(){
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/kelyAna.png"/>
+        <Avatar src="https://github.com/kelyAna.png"/>
         <strong>Ana Kely</strong>
         <span>Software Engineer</span>
       </div>
